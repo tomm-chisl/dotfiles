edae051a6f37be98cf9910fe4a162d4f50e23558
Tom <tom.b.montgomery@gmail.com>
2025-06-23 19:47:35 +0200

    Remap jk to escape

---
+++ .config/nvim/lua/plugins/colourscheme.lua
@@ -0,0 +1,13 @@
+return {
+  { "craftzdog/solarized-osaka.nvim" },
+  { "thesimonho/kanagawa-paper.nvim" },
+  { "ficcdaf/ashen.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized-osaka",
+    },
+  },
+}
