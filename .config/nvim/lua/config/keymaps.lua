8892d8d76b6a7223f93dcac6d94fdaa4a136c258
Tom <tom.b.montgomery@gmail.com>
2025-06-24 09:59:04 +0200

    fix kempays for telescope

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,6 +4,7 @@
 vim.keymap.set("i", "jk", "<esc>")
 
 -- Find files using Telescope
+local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
