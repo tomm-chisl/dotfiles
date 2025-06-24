0a34b4931edb3c66568a188aab6ffa69aec4e018
Tom <tom.b.montgomery@gmail.com>
2025-06-24 20:57:51 +0200

    add transparent background

---
+++ .config/nvim/lua/plugins/ashen.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "ficcdaf/ashen.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      colors = {
+        red_ember = "#933737",
+        background = "#2b2b2b",
+      },
+      -- transparent = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "ashen",
+    },
+  },
+}
