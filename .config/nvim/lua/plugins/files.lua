7deed543bd1f03af9950c45584664a9f92b3beb6
Tom <tom.b.montgomery@gmail.com>
2025-06-24 10:09:24 +0200

    add telescope plugins

--- .config/nvim/lua/plugins/files.lua
+++ .config/nvim/lua/plugins/files.lua
@@ -1,4 +1,9 @@
 return {
   { "nvim-lua/plenary.nvim" },
   { "ThePrimeagen/harpoon" },
+  { "nvim-telescope/telescope.nvim" },
+  { "nvim-lua/plenary.nvim" },
+  { "nvim-treesitter/nvim-treesitter" },
+  { "nvim-tree/nvim-web-devicons", opts = {} },
+  { "BurntSushi/ripgrep" },
 }
